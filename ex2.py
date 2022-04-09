b20bf436e8c628b919399d89f49045ae4d2850d1
Dragojent <Dragojent@gmail.com>
2022-04-09 14:35:12 +0300

    proper solution

--- ex2.py
+++ ex2.py
@@ -1,8 +1,14 @@
 def extract_numbers(string):
-    return [int(i) for i in string.split() if (i[0].isdigit())]
+    value = []
+    for word in string.split():
+        try: value.append(int(word))
+        except: pass
+        
+    return value
+        
 
 def main():
-    a = "asd 12 sdf 34 gf 567 fd 8 9"
+    a = "1asd 12 sdf1 34 gf 567 fd 8 9"
     print(extract_numbers(a))
 
 
